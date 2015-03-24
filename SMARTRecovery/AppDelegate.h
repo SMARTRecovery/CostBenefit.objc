a148599936080f872709798a9821b2dd9bbf8d35
Aaron Schachter <aaronschachter@gmail.com>
2015-03-23 21:14:20 -0700

    Initial commit

---
+++ SMARTRecovery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SMARTRecovery
+//
+//  Created by Aaron Schachter on 3/23/15.
+//  Copyright (c) 2015 smartrecovery.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SMARTRecovery/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SMARTRecovery
+//
+//  Created by Aaron Schachter on 3/23/15.
+//  Copyright (c) 2015 smartrecovery.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
