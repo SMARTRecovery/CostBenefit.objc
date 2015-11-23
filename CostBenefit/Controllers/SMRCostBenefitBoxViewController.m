b4a41e6a76651c36a14f7312cf413b981eddb5ac
Aaron Schachter <aaronschachter@gmail.com>
2015-11-22 21:31:13 -0800

    Adds skeleton CostBenefitBox VC's

--- CostBenefit/Controllers/SMRCostBenefitBoxViewController.m
+++ CostBenefit/Controllers/SMRCostBenefitBoxViewController.m
@@ -10,6 +10,8 @@
 
 @interface SMRCostBenefitBoxViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *boxHeaderLabel;
+
 @end
 
 @implementation SMRCostBenefitBoxViewController
@@ -29,21 +31,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    self.boxHeaderLabel.text = [NSString stringWithFormat:@"Box number %@", self.boxNumber];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- CostBenefit/Controllers/SMRCostBenefitViewController.m
+++ CostBenefit/Controllers/SMRCostBenefitViewController.m
@@ -11,6 +11,7 @@
 
 @interface SMRCostBenefitViewController () <UIPageViewControllerDataSource>
 
+@property (strong, nonatomic) NSMutableArray *boxViewControllers;
 @property (strong, nonatomic, readwrite) SMRCostBenefit *costBenefit;
 @property (strong, nonatomic) UIPageViewController *pageViewController;
 
@@ -26,6 +27,13 @@
     if (self) {
         _costBenefit = costBenefit;
         _managedObjectContext = managedObjectContext;
+        int i = 0;
+        _boxViewControllers = [[NSMutableArray alloc] init];
+        for (NSArray *boxItems in [self.costBenefit fetchBoxes:self.managedObjectContext]) {
+            SMRCostBenefitBoxViewController *boxVC = [[SMRCostBenefitBoxViewController alloc] initWithBoxNumber:[NSNumber numberWithInt:i] costBenefitItems:boxItems managedObjectContext:self.managedObjectContext];
+            i++;
+            [_boxViewControllers addObject:boxVC];
+        }
     }
 
     return self;
@@ -41,8 +49,37 @@
     self.pageViewController = [[UIPageViewController alloc] initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal options:nil];
     self.pageViewController.dataSource = self;
     [self.pageViewController view].frame = [[self view] bounds];
-
+    [self.pageViewController setViewControllers:[NSArray arrayWithObject:self.boxViewControllers[0]] direction:UIPageViewControllerNavigationDirectionForward animated:YES completion:nil];
+    [self.pageViewController addChildViewController:self.boxViewControllers[0]];
+    [[self view] addSubview:[self.pageViewController view]];
+    [self.pageViewController didMoveToParentViewController:self];
 }
 
+#pragma mark - UIPageViewControllerDataSource
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController {
+    SMRCostBenefitBoxViewController *boxVC = (SMRCostBenefitBoxViewController *)viewController;
+    if (boxVC.boxNumber.intValue == 3) {
+        return nil;
+    }
+    return self.boxViewControllers[boxVC.boxNumber.intValue + 1];
+}
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController {
+    SMRCostBenefitBoxViewController *boxVC = (SMRCostBenefitBoxViewController *)viewController;
+    if (boxVC.boxNumber.intValue == 0) {
+        return nil;
+    }
+    return self.boxViewControllers[boxVC.boxNumber.intValue - 1];
+}
+
+- (NSInteger)presentationIndexForPageViewController:(UIPageViewController *)pageViewController {
+    SMRCostBenefitBoxViewController *boxVC = (SMRCostBenefitBoxViewController *)[pageViewController.viewControllers objectAtIndex:0];
+    return [boxVC.boxNumber integerValue];
+}
+
+- (NSInteger)presentationCountForPageViewController:(UIPageViewController *)pageViewController {
+    return 4;
+}
 
 @end
